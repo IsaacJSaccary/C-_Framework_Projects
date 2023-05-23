16f275b753ca4eff74d24d44f4461f436515b034
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-05-23 19:33:42 -0400

    Car insurance

---
+++ Car_Insurance/Car_Insurance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Insurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you?");
+            int Nage = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? (Yes or no)");
+            string answer = Console.ReadLine().ToLower();
+            
+            bool dui = answer == "yes";
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tic = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Qualified for insurance?");
+            bool qualified = (Nage > 15) && !dui && (tic <= 3);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
