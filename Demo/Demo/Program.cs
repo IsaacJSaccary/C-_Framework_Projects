327ff9cd8362d98eb23ff1b6beceb23593f31bef
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-05-24 19:04:32 -0400

    While and Do While

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 6;
+            int x = 3;
+            int y = 4;
+            while (x < i)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+
+            do
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+            while (i > y);
+            Console.ReadLine();
+        }
+    }
+}
