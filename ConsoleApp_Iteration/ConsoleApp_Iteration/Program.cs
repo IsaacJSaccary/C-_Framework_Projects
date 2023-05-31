6b9d91ba1b22520509e25207e6c40bf1e438fb99
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-05-31 15:11:25 -0400

    Console App Iteration

---
+++ ConsoleApp_Iteration/ConsoleApp_Iteration/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Iteration
+{
+    class Program
+    {
+        static void Main()
+        {
+            string newname = Console.ReadLine();
+            string[] names = { "Jesse", "Sam", "Cole", "Steven" };
+            Console.WriteLine("Please enter your name:");
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i] + newname[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
