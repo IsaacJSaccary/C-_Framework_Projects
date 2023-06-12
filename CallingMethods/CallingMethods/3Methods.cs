623fba81ab24cca9f55d37f877983d0da31558db
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-06-12 14:53:11 -0400

    Calling Methods

---
+++ CallingMethods/CallingMethods/3Methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Time
+    {
+        public int Days = 365;
+        public int Months = 12;
+        public int Seconds = 31536000;
+
+    }
+}
+
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            int DaysAge;            
+            
+        }
+    }
+}
