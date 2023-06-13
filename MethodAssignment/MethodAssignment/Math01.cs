3c4e678e04e20975e35d800b0f6c11794339587e
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-06-13 19:55:31 -0400

    Method Assignment

---
+++ MethodAssignment/MethodAssignment/Math01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class Math01
+    {
+        public int MathOp(int i, int j = 1)
+        {
+            int result = i * j;
+            return result;
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Math01 math01 = new Math01();
+            Console.WriteLine("Enter an integer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second integer, or just press enter :");
+            try
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int results = math01.MathOp(num1, num2);
+                Console.WriteLine(num1 + " x " + num2 + " = " + results);
+            }
+            catch
+            {
+                int results = math01.MathOp(num1);
+                Console.WriteLine(num1 + " x Default 1 = " + results);
+            }
+            Console.ReadLine();
+        }
+    }
+}
