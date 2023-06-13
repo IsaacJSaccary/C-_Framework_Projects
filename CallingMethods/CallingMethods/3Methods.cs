db6882b3e7cb47268ff30a7cc9111845542d14c4
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-06-13 18:11:20 -0400

    File update

---
+++ CallingMethods/CallingMethods/3Methods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Time
+    {
+        public int addTen(int num1)
+        {
+            int answer = num1 + 10;
+            return answer;
+        }
+        public int addTwenty(int num2)
+        {
+            int answer = num2 + 20;
+            return answer;
+        }
+        public int addThirty(int num3)
+        {
+            int answer = num3 + 30;
+            return answer;
+        }
+
+    }
+}
+
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Time newObject = new Time();
+            Console.WriteLine("What is your favourite number");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+            int favNumPlusTen = newObject.addTen(favNum);
+            int favNumPlusTwenty = newObject.addTwenty(favNum);
+            int favNumPlusThirty = newObject.addThirty(favNum);
+            Console.WriteLine(favNumPlusTen);
+            Console.WriteLine(favNumPlusTwenty);
+            Console.WriteLine(favNumPlusThirty);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
