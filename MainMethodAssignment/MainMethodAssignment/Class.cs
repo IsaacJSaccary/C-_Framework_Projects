ca8e264371676c1ee86e88df62069e11d541b095
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-06-13 19:14:55 -0400

    Math Method Assignment

---
+++ MainMethodAssignment/MainMethodAssignment/Class.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class math1
+    {
+        public int SubFive(int num1)
+        {
+            return num1 - 5;
+            
+        }
+        public int SubFive(decimal num1)
+        {
+            int newnum = Convert.ToInt32(num1);
+            return newnum / 2;
+        }
+        public int SubFive(string num1)
+        {
+            int newnum = Convert.ToInt32(num1);
+            return newnum * 63;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            math1 math1 = new math1();
+            Console.WriteLine(math1.SubFive(100));
+            Console.WriteLine(math1.SubFive(100.65m));
+            Console.WriteLine(math1.SubFive("100"));
+            Console.ReadLine();
+        }
+    }
+}
