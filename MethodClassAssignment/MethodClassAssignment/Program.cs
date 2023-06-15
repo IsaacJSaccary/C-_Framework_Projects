3982c9bbe89514937b9bf2d2fe449f859ab4a5ee
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-06-15 19:53:03 -0400

    Method Class Assignment

---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Math01 math01 = new Math01();
+            Console.WriteLine("Pick an integer");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pick another integer");
+            try
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int result = math01.MathOp(num1, num2);
+                Console.WriteLine(num1 + num2 + result);
+            }
+            catch
+            {
+                int result = Math01.mathOp(num1);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
