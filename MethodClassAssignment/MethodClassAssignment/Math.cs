90c52738931fe72ce12463605b9f4418729669b3
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-06-20 19:36:39 -0400

    File Update

---
+++ MethodClassAssignment/MethodClassAssignment/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    public class Math01
+    {
+        public void MathOp(int num1, int num2)
+        {
+            //Creating the method MathOp
+            int result = num1 + 7;
+            Console.WriteLine(num2);
+        }
+    }
+}
