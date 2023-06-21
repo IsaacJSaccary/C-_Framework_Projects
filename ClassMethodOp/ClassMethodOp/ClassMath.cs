8da24f4c92196ef09e28f039e76de593bd35506d
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-06-20 21:51:20 -0400

    Class Method Operation

---
+++ ClassMethodOp/ClassMethodOp/ClassMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodOp
+{
+    public class ClassMath
+    {
+        //Created the MathOp method for int num1
+        public void MathOp(int num1)
+        {
+            int result = num1 / 2;
+            Console.WriteLine(result);
+        }
+        //Created the MathOp method for decimal num2
+        public static void MathOp(decimal num2 = 8)
+        {
+            decimal results = num2 + 7;
+            Console.WriteLine(results);
+        }
+    }
+}
---
+++ ClassMethodOp/ClassMethodOp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodOp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Created the operation output
+            ClassMath classmath = new ClassMath();
+            //Created num2 to display output parameters
+            decimal num2 = 8;
+            //Created the group of text to make num1 a user input
+            Console.WriteLine("Enter an integer");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            //Called the methods
+            classmath.MathOp(num1);
+            ClassMath.MathOp(num2);
+            Console.ReadLine();
+        }
+    }
+}
