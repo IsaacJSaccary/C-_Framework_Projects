c18098a106d795b725c895c8fe3ea44559ba2426
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-06-21 20:39:17 -0400

    Methods and Objects assignment
    Inheriting class data

---
+++ MethodsAndObjects/MethodsAndObjects/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    public class Person
+    {
+        //Created data type string FirstName and LastName
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //Created void method SayName() to display the full name to the console
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + LastName);
+        }
+    }
+}
---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate Employee object
+            Employee name = new Employee();
+            name.FirstName = "Sample ";
+            name.LastName = "Student";
+            //Called the super class method SayName()
+            name.SayName();
+            Console.ReadLine();
+        }
+    }
+}
