64f458a2db13cbdb8ca6e9d1480a993b3c42d429
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-06-27 15:14:17 -0400

    Abstract Class updated

--- AbstractClass/AbstractClass/Employee.cs
+++ AbstractClass/AbstractClass/Employee.cs
@@ -16,7 +16,7 @@ namespace AbstractClass
         //Employee class inherit the interface and implement the Quit() method
         public void IQuit(Employee employee)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("I have successfully created and inherited an interface.");
         }
     }
 }
