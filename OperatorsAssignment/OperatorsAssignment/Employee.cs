d75e9d2592f8d7ef8d5b852a51d582ec7f6edaf2
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-07-04 19:33:41 -0400

    Operators assignment

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    { 
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int Id { get; set; }
+
+        public static Employee operator== (Employee a, Employee b)
+        {
+            return a == b;
+        }
+        public static Employee operator!= (Employee a, Employee b) 
+        {
+            return a != b;
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+                
+                employee.firstName = "John";
+                employee.lastName = "Smith";
+                employee.Id = 1;
+                
+                employee.firstName = "Smith";
+                employee.lastName = "John";
+                employee.Id = 2;
+            
+            Console.ReadLine();
+
+            
+        }
+    }
+}
