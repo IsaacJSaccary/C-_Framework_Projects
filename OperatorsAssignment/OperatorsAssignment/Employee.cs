9450dccea21af667964ee88b68fd067a557c6c23
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-07-05 14:33:27 -0400

    Operators assignment

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    //Create an Employee class
+    public class Employee
+    { 
+        //With FirstName
+        public string firstName { get; set; }
+        //LastName
+        public string lastName { get; set; }
+        //And Id properties.
+        public int Id { get; set; }
+        //Overload the "==" operator so it checks if two employee ebjects are equal by comparing their Id.
+        public static bool operator== (Employee a, Employee b)
+        {
+            return a.Id == b.Id;
+        }
+        public static bool operator!= (Employee a, Employee b) 
+        {
+            return a.Id != b.Id;
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate two objects of the Employee class and assign values to their properties.
+            Employee employee1 = new Employee();
+                
+                employee1.firstName = "John";
+                employee1.lastName = "Smith";
+                employee1.Id = 1;
+
+            Employee employee2 = new Employee();
+            employee2.firstName = "Smith";
+            employee2.lastName = "John";
+            employee2.Id = 2;
+
+            Console.WriteLine(employee1 == employee2);
+            
+            Console.ReadLine();
+
+            
+        }
+    }
+}
