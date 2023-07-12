b91a48616304344c8de6db7502fca45df8a95b98
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-07-12 17:24:50 -0400

    Struct Assignment

---
+++ StructAssignment/StructAssignment/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    //Create a struct called Number and give it the property Amount
+     public struct Number
+    {
+       public decimal Amount { get; set; }
+    }
+}
