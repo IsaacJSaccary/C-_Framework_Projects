d3b9b528167c148601913e29bb97e3f6f586e546
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-07-21 21:14:47 -0400

    Input Assignment

---
+++ InputAssignment01/InputAssignment01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create the statement and int answer will hold the value
+            Console.WriteLine("Please enter a number");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            //Create the StreamWriter which will send the value to the text file
+            using (StreamWriter file = new StreamWriter(@"C:\Users\isaac\Logs.txt", true))
+            {
+                file.WriteLine(answer);
+            }
+        }
+    }
+}
