5d2c3ef151b0badd3d9b057fa6fd597d43165a7e
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-07-21 21:41:23 -0400

    Date Time Assignment

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print the current date and time to the console
+            Console.WriteLine("The current time is: " + DateTime.Now);
+            //Ask for a number
+            Console.WriteLine("Enter a number");
+            float answer = float.Parse(Console.ReadLine());
+            //Prints the time and date it will be in X amount of hours
+            Console.WriteLine("In " + answer + " hours it will be " + DateTime.Now.AddHours(answer));
+            Console.ReadLine();
+        }
+    }
+}
