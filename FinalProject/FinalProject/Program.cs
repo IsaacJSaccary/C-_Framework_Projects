625e7385137b90fb2bf8b7a8a0aa19721840c88f
IsaacJSaccary <118865604+IsaacJSaccary@users.noreply.github.com>
2023-08-21 21:04:03 -0400

    Update

---
+++ FinalProject/FinalProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace FinalProject
+{ 
+    class Program
+    {
+        static void Main()
+        {
+            using (var ctx = new SchoolContext())
+
+            {
+                var stud = new Student() { FirstName = "Isaac", LastName = "Saccary", Age = 22 };
+
+                ctx.Students.Add(stud);
+                ctx.Students.SaveChanges();
+                return;
+            }
+        }
+    }
+}
